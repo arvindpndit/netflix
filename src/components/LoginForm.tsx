77c40fd1321e738f165c4825e5d49c0d509e8e3a
arvindpndit <arvindpandit9808@gmail.com>
2023-10-14 10:57:16 +0530

    toggle login form added

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
-const LoginForm : React.FC = () => {
+interface Props {
+  toggleLoginForm: boolean,
+  setToggleLoginForm:React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const LoginForm : React.FC<Props> = (props) => {
+  const {toggleLoginForm, setToggleLoginForm} = props;
   return (
     <div className="w-11/12 md:w-4/12 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-8 bg-black text-white rounded-lg bg-opacity-80 shadow-lg">
-      <h1 className="text-3xl font-bold mb-4">Sign In</h1>
+      <h1 className="text-3xl font-bold mb-4">
+        {toggleLoginForm ? "Sign In" : "Sign Up"}
+      </h1>
+      {
+        !toggleLoginForm &&
+        <input
+        type="text"
+        placeholder="Full Name"
+        className="w-full py-2 px-3 rounded-lg mb-4 bg-gray-800 text-white placeholder-gray-400"
+        />
+      }
       <input
         type="text"
         placeholder="Email or phone number"
@@ -15,10 +31,18 @@ const LoginForm : React.FC = () => {
         className="w-full py-2 px-3 rounded-lg mb-4 bg-gray-800 text-white placeholder-gray-400"
       />
       <button className="w-full py-2 bg-red-600 hover:bg-red-700 text-white rounded-lg font-semibold mb-4">
-        Sign In
+        {toggleLoginForm ? "Sign In" : "Sign Up"}
       </button>
-      <p className="text-sm text-gray-300 cursor-pointer">
-        New to Netflix-GPT? <span className="text-red-500">Sign up now.</span>
+      <p className="text-sm text-gray-300 cursor-pointer" onClick={() => setToggleLoginForm(!toggleLoginForm)}>
+        {toggleLoginForm ? (
+          <>
+            New to Netflix-GPT? <span className="text-red-500">Sign up now.</span>
+          </>
+        ) : (
+          <>
+            Already a user? <span className="text-red-500">Sign in now.</span>
+          </>
+        )}
       </p>
     </div>
   )
--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Header from './Header'
 import LoginForm from './LoginForm'
 
 const LoginPage : React.FC = () => {
+  const [toggleLoginForm, setToggleLoginForm] = useState<boolean>(true);
   return (
     <div className='relative'>
       <Header/>
       <img className='brightness-50' src="https://assets.nflxext.com/ffe/siteui/vlv3/ab180a27-b661-44d7-a6d9-940cb32f2f4a/7fb62e44-31fd-4e1f-b6ad-0b5c8c2a20ef/IN-en-20231009-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="" />
-      <LoginForm/>
+      <LoginForm toggleLoginForm={toggleLoginForm} setToggleLoginForm={setToggleLoginForm}/>
     </div>
   )
 }
