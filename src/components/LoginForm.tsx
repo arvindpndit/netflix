5bc99026d1b76ca4208861bc507cfb3b813ecaa6
arvindpndit <arvindpandit9808@gmail.com>
2023-10-14 10:30:59 +0530

    added sign in component

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const LoginForm : React.FC = () => {
   return (
-    <div>LoginForm</div>
+    <div className="w-11/12 md:w-4/12 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-8 bg-black text-white rounded-lg bg-opacity-80 shadow-lg">
+      <h1 className="text-3xl font-bold mb-4">Sign In</h1>
+      <input
+        type="text"
+        placeholder="Email or phone number"
+        className="w-full py-2 px-3 rounded-lg mb-4 bg-gray-800 text-white placeholder-gray-400"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        className="w-full py-2 px-3 rounded-lg mb-4 bg-gray-800 text-white placeholder-gray-400"
+      />
+      <button className="w-full py-2 bg-red-600 hover:bg-red-700 text-white rounded-lg font-semibold mb-4">
+        Sign In
+      </button>
+      <p className="text-sm text-gray-300 cursor-pointer">
+        New to Netflix-GPT? <span className="text-red-500">Sign up now.</span>
+      </p>
+    </div>
   )
 }
 
--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -5,7 +5,7 @@ import LoginForm from './LoginForm'
 
 const LoginPage : React.FC = () => {
   return (
-    <div>
+    <div className='relative'>
       <Header/>
       <img className='brightness-50' src="https://assets.nflxext.com/ffe/siteui/vlv3/ab180a27-b661-44d7-a6d9-940cb32f2f4a/7fb62e44-31fd-4e1f-b6ad-0b5c8c2a20ef/IN-en-20231009-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="" />
       <LoginForm/>
