ffad91c2d905e1620f36ccb44123d8b50c742b33
arvindpndit <arvindpandit9808@gmail.com>
2023-10-21 22:16:29 +0530

    add signout route

--- src/components/BrowsePage.tsx
+++ src/components/BrowsePage.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Header from './Header'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { removeUser } from '../redux/userSlice'
 
 const BrowsePage : React.FC = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+    dispatch(removeUser())
+  }
+
   return (
-    <div>BrowsePage</div>
+    <div>
+      <div >
+        <Header/>
+        <button className=" mt-48 p-2 bg-red-600" onClick={handleClick}>Sign out</button>
+      </div>
+    </div>
   )
 }
 
