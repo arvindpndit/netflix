314f5e0f344f70dfa839711cad54cf9d4b41c8bf
arvindpndit <arvindpandit9808@gmail.com>
2023-10-26 22:31:48 +0530

    refactor: extract the signup btn from brose page to header

--- src/components/BrowsePage.tsx
+++ src/components/BrowsePage.tsx
@@ -1,35 +1,17 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
 
-import { removeUser } from "../redux/userSlice";
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import BrowseHeroContainer from "./BrowseHeroContainer";
 import BrowseMoviesCardsList from "./BrowseMoviesCardsList";
 
 const BrowsePage: React.FC = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
   useNowPlayingMovies();
-
-  const handleClick = (): void => {
-    navigate("/");
-    dispatch(removeUser());
-  };
 
   return (
     <div>
       <div>
         <Header />
-        <button
-          className="mt-4 ml-96 z-50 p-2 bg-red-600 text-white hover:bg-red-700 absolute"
-          onClick={handleClick}
-        >
-          Sign out
-        </button>
-
         <BrowseHeroContainer />
         <BrowseMoviesCardsList />
       </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-const Header:React.FC  = () => {
+import { removeUser } from "../redux/userSlice";
+import { RootState } from "../redux/store";
+
+const Header: React.FC = () => {
+  const user = useSelector((state: RootState) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleClick = (): void => {
+    navigate("/");
+    dispatch(removeUser());
+  };
+
   return (
-    <div className='h-24 w-full z-50 absolute bg-gradient-to-b from-black '>
-      <img className='h-full px-3' src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"/>
-    </div>
-  )
-}
+    <div className="h-24 w-full z-50 absolute bg-gradient-to-b from-black ">
+      <img
+        className="h-full px-3"
+        src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+      />
 
-export default Header
+      {user === null ? null : (
+        <button
+          className="mt-4 ml-96 z-50 p-2 bg-red-600 text-white hover:bg-red-700 absolute"
+          onClick={handleClick}
+        >
+          Sign out
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Header;
